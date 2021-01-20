b8ee8ecd55fdf4b899eb53433a9362f18b20220d
jasmineharris <jharris@linkedin.com>
2021-01-19 17:00:26 -0800

    Add doc strings, more testing, xy coordinate outline

--- game/game.py
+++ game/game.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 conn = sqlite3.connect('tictactoescore.db')
 c = conn.cursor()
 
@@ -10,6 +11,11 @@ c.execute("""CREATE TABLE IF NOT EXISTS scores (
 
 
 def insert_scores(player_name):
+    """
+    Takes in a string containing winning player's name. If the name does not exist in the database, insert
+    the name into the database with 1 win. Otherwise, update the wins associated with the name by 1.
+    :param player_name: string containting name of winning player 
+    """
     c.execute("""SELECT name FROM scores WHERE name=?""",(player_name,))
     result = c.fetchone()
 
@@ -22,8 +28,10 @@ def insert_scores(player_name):
             c.execute("INSERT INTO scores VALUES (?, ?)", (player_name, 1))
     
 
-# TODO clean up scoreboard output
 def print_scoreboard():
+    """
+    Fetches the top 5 five scoring players from the database and displays them in descending order
+    """
     print("***************************************")
     print("               SCOREBOARD              ")
 
@@ -32,43 +40,75 @@ def print_scoreboard():
             print(c.fetchall())
     print("***************************************")
 
+
 def draw_board(board):
+    """
+    Takes in board, extracts size by the length of the board, and prints out tic-tac-toe board with
+    boarder displaying coordinate instructions
+    :param board: 2D array containing character values for moves or " " for empty space
+    """
     board_size = len(board)
+    print("0,0", end="")
+    for _ in range(board_size):
+        print("--", end="")
+    print("> Y")
+    print("| ")
     for row_idx, row in enumerate(board):
         for col_idx, value in enumerate(row):
+            if col_idx == 0:
+                print("|  ", end="")
             print(f"{value}", end ="")
             if col_idx != board_size - 1:
                 print("|", end ="")
         print("")
         if row_idx != board_size - 1:
+            print("|  ", end="")
             print("_+"* (board_size - 1), end ="")
-            print("_")        
+            print("_")   
+    print("V")
+    print("X\n")      
 
 
 def take_next_move(character, board, player_name):
-    move = input(f"Your turn, {player_name}. Enter your next move with x,y coordinates: ")
+    """
+    Takes in string input from the user in the form of X,Y coordinates. Checks if move input is empty, has letters, has a comma, 
+    exists on the board, is already already taken. If move input passes checks, assign character to corresponding space on board.
+    :param character: 1 element string with character assigned for play 
+    :param board: 2D array containing character values for moves or " " for empty space
+    :param player_name: string with name of currentplayer
+    """
+    move = input(f"Your turn, {player_name} with character {character}. Enter your next move with x,y coordinates: ")
+    #retry if empty string
     if move == "":
         print("PLEASE ENTER YOUR MOVE XY COORDINATES SEPARATED BY A COMMA.")
         return take_next_move(character, board, player_name)
+    #retry if string contains letter
     if is_move_a_digit(move) is False:
         print("PLEASE ENTER NUMERIC XY COORDINATES SEPARATED BY A COMMA")
         return take_next_move(character, board, player_name)
+    #retry if comma is not entered
     if "," not in move:
         print("PLEASE ENTER NUMERIC XY COORDINATES SEPARATED BY A COMMA")
         return take_next_move(character, board, player_name)
     row_idx, col_idx = clean_up_coordinates(move)
+    #check if move exists on board
     if is_valid_move(row_idx, col_idx, board) is False:
         print("INPUT OUT OF BOUNDS. PLEASE ENTER YOUR MOVE XY COORDINATES SEPARATED BY A COMMA.")
         return take_next_move(character, board, player_name)
+    #check if move is already taken
     if board[row_idx][col_idx] != " ":
         print("MOVE ALREADY TAKEN ON THE BOARD. PLEASE RETRY.")
         return take_next_move(character, board, player_name)
     #add player move to board
     board[row_idx][col_idx] = character
-    return character
     
 
 def is_move_a_digit(move):
+    """
+    Takes in string input and determines if the string contains alphabetic characters
+    :param move: string 
+    :return: True if input does not contain alphabetic characters, False if contains alphabetic characters. 
+    """
     for value in move:
         if value.isalpha():
             return False
@@ -76,17 +116,22 @@ def is_move_a_digit(move):
 
 
 def clean_up_coordinates(move):
+    """
+    Takes in coordinates in the forms, transforms into a list, cleans up white space. If empty string, set to -1 to flag
+    as out of bounds. 
+    :param move: string
+    :return: x, y coordinates cleaned up to be used for input in take_move
+    """
     coordinates = move.split(",")
-    row_idx = coordinates[0]
-    if len(coordinates) < 2:
-        col_idx = None
-    else:
-        col_idx = coordinates[1]
-    row_idx = coordinates[0].strip()
-    col_idx = coordinates[1].strip()
-    row_idx = int(row_idx)
-    col_idx = int(col_idx)
-    return row_idx, col_idx
+    clean_coordinates = []
+    for coordinate in coordinates:
+        coordinate = coordinate.strip()
+        if coordinate == "":
+            # flag to catch as out of bounds
+            coordinate = -1
+        coordinate = int(coordinate)
+        clean_coordinates.append(coordinate)
+    return clean_coordinates[0], clean_coordinates[1]
 
 
 # west = board[row_idx][col_idx - 1]
@@ -98,6 +143,13 @@ def clean_up_coordinates(move):
 
 
 def check_if_player_won(board, character, player_name):
+    """
+    Checks if players has won the game
+    :param character: 1 element string with character assigned for play 
+    :param board: 2D array containing character values for moves or " " for empty space
+    :param player_name: string with name of currentplayer
+    :return: string declaring winner if player has won or draw if there is no declared winner
+    """
     board_size = len(board)
     #check going west to east
     for row_idx, row in enumerate(board):
@@ -142,15 +194,31 @@ def check_if_player_won(board, character, player_name):
 
 
 def is_valid_move(row_idx, col_idx, board):
+    """
+    Checks if the move the player entered exists within the bounds of the board
+    :param row_idx: x coordinate
+    :param col_idx: y coordinate
+    :param board: 2D array containing character values for moves or " " for empty space
+    """
     return row_idx >= 0 and row_idx < len(board) and col_idx >= 0 and col_idx < len(board)
 
-
+ 
 def play_again():
+    """
+    Checks if player would like to play another game
+    :return: True if 'y' or False if 'y' is not input
+    """
     play = input("Would you like to play again? Enter yes or no: ")
     return play.lower().startswith('y')
 
 
 def retrieve_player_information(game_participants):
+    """
+    Takes in player name at the start of the game and assigns player number and game character
+    :param game_participants: Integer representing number of players
+    :return: Dictionary with the key is the player number and the value is a dictionary 
+    containing the name of player and their gameplay character.
+    """
     character_set = ['X', 'O', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N']
     players_dict = {}
     for participant in range(game_participants):
@@ -163,16 +231,24 @@ def retrieve_player_information(game_participants):
 
 
 def comp_move(board):
+    """
+    Computer player
+    :param board: 2D array containing character values for moves or " " for empty space
+    """
     for row_idx, row in enumerate(board):
         for col_idx, value in enumerate(row):
             if board[row_idx][col_idx] == " ":
                 board[row_idx][col_idx] = 'O'
-                return 'O'
+                return 
 
-def multiplayer_game():       
+
+def multiplayer_game():
+    """
+    Controller for multiplayer game
+    """       
     board_size = int(input("Enter your tic tac toe board size as a single digit(ex. Enter 3 for 3x3): "))
     board = [[" " for yaxis in range(board_size)] for xaxis in range(board_size)]
-    game_participants = 2
+    game_participants = int(input("How many players?: "))
     players_dict = retrieve_player_information(game_participants)
     gameplay = True
     while gameplay:
@@ -181,7 +257,7 @@ def multiplayer_game():
             player_name = players_dict[player_no]["name"]
             character = players_dict[player_no]["character"]
             draw_board(board)        
-            character = take_next_move(character, board, player_name)
+            take_next_move(character, board, player_name)
             player_winner = check_if_player_won(board, character, player_name)
             if player_winner != None:
                 print(f"{player_winner}")
@@ -193,6 +269,9 @@ def multiplayer_game():
 
 
 def computer_game():
+    """
+    Controller for computer gameplay
+    """
     board_size = int(input("Enter your tic tac toe board size as a single digit(ex. Enter 3 for 3x3): "))
     board = [[" " for yaxis in range(board_size)] for xaxis in range(board_size)]
     game_participants = 1
@@ -224,6 +303,9 @@ def computer_game():
 
 
 def run_game():
+    """
+    Gameplay loop
+    """
     print("Welcome to Tic Tac Toe! ", end="")
     reset_board = True
     while reset_board:
--- test/test_game.py
+++ test/test_game.py
@@ -1,6 +1,8 @@
 import unittest
 import pytest
+from unittest.mock import patch
 from game.game import *
+
 
 @pytest.fixture()
 def board():
@@ -8,51 +10,57 @@ def board():
 
 
 def test_if_selected_move_is_on_the_board(board):
-    assert False == is_valid(-1, -1, board)
-    assert True == is_valid(0, 0, board)
-    assert True == is_valid(1, 1, board)
-    assert False == is_valid(3, 0, board)
-    assert False == is_valid(0, 3, board)
-    assert False == is_valid(0, -1, board)
+    assert False == is_valid_move(-1, -1, board)
+    assert True == is_valid_move(0, 0, board)
+    assert True == is_valid_move(1, 1, board)
+    assert False == is_valid_move(3, 0, board)
+    assert False == is_valid_move(0, 3, board)
+    assert False == is_valid_move(0, -1, board)
 
 
 def test_can_somone_win_horizontally(board):
     board[0] = ['X', 'X', 'X']
-    assert "winner is X" == check_if_player_won(board, "X")
+    assert "winner is Jasmine" == check_if_player_won(board, "X", "Jasmine")
 
 
 def test_can_someone_win_vertically(board):
     board[0][0] = 'X'
     board[1][0] = 'X'
     board[2][0] = 'X'
-    assert "winner is X" == check_if_player_won(board, "X")
+    assert "winner is Jasmine" == check_if_player_won(board, "X", "Jasmine")
 
 
 def test_can_someone_win_descending_diagonally(board):
     board[0][0] = 'X'
     board[1][1] = 'X'
     board[2][2] = 'X'
-    assert "winner is X" == check_if_player_won(board, "X")
+    assert "winner is Jasmine" == check_if_player_won(board, "X", "Jasmine")
 
 
 def test_can_someone_win_ascending_diagonally(board):
     board[0][2] = 'X'
     board[1][1] = 'X'
     board[2][0] = 'X'
-    assert "winner is X" == check_if_player_won(board, "X")
+    assert "winner is Jasmine" == check_if_player_won(board, "X", "Jasmine")
 
-
-# def test_can_a_player_override_another_player(board):
-#     board[0][0] = 'X'
+        
+def test_play_again():
+    with patch('builtins.input', return_value="yes") as mock_input:
+        assert play_again() is True
     
+    with patch('builtins.input', return_value="no") as mock_input:
+        assert play_again() is False
 
-def test_retrive_player_information():
-    pass
+
+@patch('builtins.input', side_effect=["Jasmine", "Kyle", "Lulu"])
+def test_retrive_player_information(mock_input):
+    expected = {1: {'name': 'Jasmine', 'character': 'X'}, 2: {'name': 'Kyle', 'character': 'O'}, 3: {'name': 'Lulu', 'character': 'A'}}
+    assert expected == retrieve_player_information(3)
 
 
 def test_draw():
     board = [['X', 'O', 'X'], ['X', 'O', 'X'], ['O', 'X', 'O']]
-    assert "no winner, draw game" == check_if_player_won(board, "X")
+    assert "no winner, draw game" == check_if_player_won(board, "X", "Jasmine")
 
 
 def test_clean_up_coordinates():
